d519d22e713f639f89af09ded21c0a0eaef4ee56
Finkons <91851050+Finkons@users.noreply.github.com>
2022-06-03 20:52:39 +0000

    Deploying to gh-pages from @ Finkons/guess-the-number@f1736f93879087b5681b41e75fc6ba38437befa2 🚀

--- index.476aa686.js
+++
@@ -1,2 +0,0 @@
-let o,l=0;const e=1+~~(100*Math.random());do{l++,o=Number(prompt("enter your number")),console.log(o),e>o?console.log("x >",o):e<o?console.log("x <",o):console.log("Bingo!!!")}while(e!==o);console.log("count:",l);
-//# sourceMappingURL=index.476aa686.js.map
