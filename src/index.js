f1736f93879087b5681b41e75fc6ba38437befa2
Andrey <andrey@MacBook-Air-Andrey.local>
2022-06-03 23:51:25 +0300

    add function onCheck

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
-//1 -- > 100
 
-let count = 0;
-let userInput;
+let count = '';
 const x = ~~(Math.random() * 100) + 1;
-// // console.log(x);
 
-do {
+const STORAGE_KEY = 'Best Score';
+
+const refs = {
+  input: document.querySelector('.input'),
+  button: document.getElementById('button'),
+  resultText: document.querySelector('.result-text'),
+  message: document.querySelector('#game-message'),
+  bingo: document.querySelector('.bingo')
+}
+
+const { input, button, resultText, message, bingo } = refs
+
+button.addEventListener('click', onCheck)
+
+function onCheck() {
+  const onCheckInput = input.value.trim()
   count++;
-  userInput = Number(prompt('enter your number'));
-  console.log(userInput);
-
-  if (x > userInput) {
-    console.log('x >', userInput);
-  } else if (x < userInput) {
-    console.log('x <', userInput);
-  } else {
-    console.log('Bingo!!!');
+  console.log(onCheckInput);
+  if (onCheckInput > 100) {
+    message.textContent = 'Enter a number less than 100'
+  } else if (onCheckInput <= 0) {
+    message.textContent = 'Enter a number more than 0'
+  } else if (x > onCheckInput) {
+    message.textContent = 'more'
+  } else if (x < onCheckInput) {
+    message.textContent = 'less'
   }
-} while (x !== userInput);
+  else {
+    saveResult()
+  }
+  console.log('count:', count);
+}
+const bestScore = JSON.parse(localStorage.getItem(STORAGE_KEY))
+function saveResult() {
+  message.classList.add('bingo')
+  message.textContent = 'Bingo!!!'
+  setTimeout(() => {
+    location.reload()
+  }, 2000);
 
-console.log('count:', count);
+  sessionStorage.setItem('Current point', JSON.stringify(count))
+  if (count < bestScore) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(count))
+    resultText.textContent = `${bestScore}`;
+  }
+}
+resultText.textContent = `${bestScore}`;
