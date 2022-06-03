a35ea1ce60aeb999cef5c9161e14e4a3beb22659
Andrey <andrey@MacBook-Air-Andrey.local>
2022-06-04 00:16:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -49,3 +49,19 @@ function saveResult() {
   }
 }
 resultText.textContent = `${bestScore}`;
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  let response = await fetch(request)
+  let newHeaders = new Headers(response.headers)
+  newHeaders.set("Permissions-Policy", "interest-cohort=()")
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: newHeaders
+  })
+}
